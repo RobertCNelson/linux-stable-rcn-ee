1590c79ca170c0c44c1e62fdc17b5bfbcdbb05da
Robert Nelson <robertcnelson@gmail.com>
2015-04-09 11:56:11 -0500

    etnaviv: wheezy build fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/staging/etnaviv/etnaviv_gem.c
+++ drivers/staging/etnaviv/etnaviv_gem.c
@@ -726,7 +726,8 @@ struct get_pages_work {
 static struct page **etnaviv_gem_userptr_do_get_pages(
 	struct etnaviv_gem_object *etnaviv_obj, struct mm_struct *mm, struct task_struct *task)
 {
-	int ret, pinned, npages = etnaviv_obj->base.size >> PAGE_SHIFT;
+	int ret = 0;
+	int pinned, npages = etnaviv_obj->base.size >> PAGE_SHIFT;
 	struct page **pvec;
 	uintptr_t ptr;
 
@@ -790,7 +791,8 @@ static int etnaviv_gem_userptr_get_pages(struct etnaviv_gem_object *etnaviv_obj)
 	struct page **pvec = NULL;
 	struct get_pages_work *work;
 	struct mm_struct *mm;
-	int ret, pinned, npages = etnaviv_obj->base.size >> PAGE_SHIFT;
+	int ret = 0;
+	int pinned, npages = etnaviv_obj->base.size >> PAGE_SHIFT;
 
 	if (etnaviv_obj->userptr.work) {
 		if (IS_ERR(etnaviv_obj->userptr.work)) {
