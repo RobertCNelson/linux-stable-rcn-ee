800be68c08551f7533e97d021c297e52d6fced45
Roger Quadros <rogerq@ti.com>
2017-02-09 12:13:21 -0600

    remoteproc/pru: Add pru_rproc_set_ctable() and pru_rproc_set_gpimode()
    Some firmwares expect the OS drivers to configure the CTABLE
    entries publishing dynamically allocated memory regions. For
    example, the PRU Ethernet firmwares use the C28 and C30 entries
    for retrieving the Shared RAM and System SRAM (OCMC) areas
    allocated by the PRU Ethernet client driver.
    
    Provide a way for users to do that through a new API,
    pru_rproc_set_ctable(). The API returns 0 on success and
    a negative value on error.
    
    NOTE:
    The programmable CTABLE entries are typically re-programmed by
    the PRU firmwares when dealing with a certain block of memory
    during block processing. This API provides an interface to the
    PRU client drivers to publish a dynamically allocated memory
    block with the PRU firmware using a CTABLE entry instead of a
    negotiated address in shared memory. Additional synchronization
    may be needed between the PRU client drivers and firmwares if
    different addresses needs to be published at run-time reusing
    the same CTABLE entry.
    
    Some client drivers need to set the GPI mode of the
    PRU. The GPI mode select bits like in the GPCFG register
    space. Add pru_rproc_set_gpimode() API to set the GPI
    mode for the PRU.
    
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    [s-anna@ti.com: add the NOTE: on patch description, minor cleanups]
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- include/linux/remoteproc/pru_rproc.h
+++ include/linux/remoteproc/pru_rproc.h
@@ -8,10 +8,28 @@
 #ifndef __LINUX_REMOTEPROC_PRU_RPROC_H
 #define __LINUX_REMOTEPROC_PRU_RPROC_H
 
+/**
+ * enum pru_ctable_idx - Configurable Constant table index identifiers
+ */
+enum pru_ctable_idx {
+	PRU_C24 = 0,
+	PRU_C25,
+	PRU_C26,
+	PRU_C27,
+	PRU_C28,
+	PRU_C29,
+	PRU_C30,
+	PRU_C31,
+};
+
+enum pruss_gpi_mode;
+
 #if IS_ENABLED(CONFIG_PRUSS_REMOTEPROC)
 
 struct rproc *pru_rproc_get(struct device_node *node, int index);
 void pru_rproc_put(struct rproc *rproc);
+int pru_rproc_set_ctable(struct rproc *rproc, enum pru_ctable_idx c, u32 addr);
+int pru_rproc_set_gpimode(struct rproc *rproc, enum pruss_gpi_mode mode);
 
 #else
 
@@ -21,6 +39,18 @@ static inline struct rproc *pru_rproc_get(struct device_node *node, int index)
 }
 
 static inline void pru_rproc_put(struct rproc *rproc) { }
+
+static inline int pru_rproc_set_ctable(struct rproc *rproc,
+				       enum pru_ctable_idx c, u32 addr)
+{
+	return -ENOTSUPP;
+}
+
+static inline int pru_rproc_set_gpimode(struct rproc *rproc,
+					enum pruss_gpi_mode mode)
+{
+	return -ENOTSUPP;
+}
 
 #endif /* CONFIG_PRUSS_REMOTEPROC */
 
