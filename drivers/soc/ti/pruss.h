5e96244d25d513a05327b08f2186a8624cb815d2
Suman Anna <s-anna@ti.com>
2018-11-22 13:39:04 +0200

    soc: ti: pruss: Add a PRUSS irqchip driver for PRUSS interrupts
    The Programmable Real-Time Unit Subsystem (PRUSS) contains an
    interrupt controller (INTC) that can handle various system input
    events and post interrupts back to the device-level initiators.
    The INTC can support upto 64 input events with individual control
    configuration and hardware prioritization. These events are mapped
    onto 10 interrupt signals through two levels of many-to-one mapping
    support. Different interrupt signals are routed to the individual
    PRU cores or to the host CPU.
    
    The PRUSS INTC platform driver manages this PRUSS interrupt
    controller and implements an irqchip driver to provide a Linux
    standard way for the PRU client users to enable/disable/ack/
    re-trigger a PRUSS system event. The system events to interrupt
    channels and host interrupts relies on the mapping configuration
    provided through a firmware resource table for now. This will be
    revisited and enhanced in the future for a better interface. The
    mappings will currently be programmed during the boot/shutdown
    of the PRU.
    
    The PRUSS INTC module is reference counted during the interrupt
    setup phase through the irqchip's irq_request_resources() and
    irq_release_resources() ops. This restricts the module from being
    removed as long as there are active interrupt users.
    
    The driver currently supports the AM335x SoC.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Signed-off-by: Roger Quadros <rogerq@ti.com>

--- drivers/soc/ti/pruss.h
+++ drivers/soc/ti/pruss.h
@@ -9,6 +9,18 @@
 #ifndef _PRUSS_H_
 #define _PRUSS_H_
 
+/* maximum number of system events */
+#define MAX_PRU_SYS_EVENTS	64
+
+/* maximum number of interrupt channels */
+#define MAX_PRU_CHANNELS	10
+
+/* minimum starting host interrupt number for MPU */
+#define MIN_PRU_HOST_INT	2
+
+/* maximum number of host interrupts */
+#define MAX_PRU_HOST_INT	10
+
 /**
  * enum pruss_mem - PRUSS memory range identifiers
  */
@@ -32,13 +44,30 @@ struct pruss_mem_region {
 };
 
 /**
+ * struct pruss_intc_config - INTC configuration info
+ * @sysev_to_ch: system events to channel mapping information
+ * @ch_to_host: interrupt channel to host interrupt information
+ */
+struct pruss_intc_config {
+	s8 sysev_to_ch[MAX_PRU_SYS_EVENTS];
+	s8 ch_to_host[MAX_PRU_CHANNELS];
+};
+
+/**
  * struct pruss - PRUSS parent structure
  * @dev: pruss device pointer
  * @mem_regions: data for each of the PRUSS memory regions
+ * @host_mask: indicate which HOST IRQs are enabled
  */
 struct pruss {
 	struct device *dev;
 	struct pruss_mem_region mem_regions[PRUSS_MEM_MAX];
+	u32 host_mask;
 };
+
+int pruss_intc_configure(struct pruss *pruss,
+			 struct pruss_intc_config *intc_config);
+int pruss_intc_unconfigure(struct pruss *pruss,
+			   struct pruss_intc_config *intc_config);
 
 #endif	/* _PRUSS_H_ */
