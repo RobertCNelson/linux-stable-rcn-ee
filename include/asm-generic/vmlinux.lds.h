c487e405d164bec14755809efaa77215ca4ab603
Robert Nelson <robertcnelson@gmail.com>
2021-03-09 08:49:20 -0600

    Revert "vmlinux.lds.h: Create section for protection against instrumentation"
    This reverts commit 4f5416710e13eb4e1587f6c38e92e9134cf5f480.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -511,15 +511,6 @@
 #define RO_DATA(align)  RO_DATA_SECTION(align)
 
 /*
- * Non-instrumentable text section
- */
-#define NOINSTR_TEXT							\
-		ALIGN_FUNCTION();					\
-		__noinstr_text_start = .;				\
-		*(.noinstr.text)					\
-		__noinstr_text_end = .;
-
-/*
  * .text section. Map to function alignment to avoid address changes
  * during second ld run in second ld pass when generating System.map
  *
@@ -533,7 +524,6 @@
 		*(TEXT_MAIN .text.fixup)				\
 		*(.text.unlikely .text.unlikely.*)			\
 		*(.text.unknown .text.unknown.*)			\
-		NOINSTR_TEXT						\
 		*(.text..refcount)					\
 		*(.ref.text)						\
 	MEM_KEEP(init.text*)						\
