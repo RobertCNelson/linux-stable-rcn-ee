caedc9f9e34b4b382378512715010b935ead5aa0
Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
2021-07-27 13:56:32 +0300

    clk: at91: sama7g5: do not use AUDIOPLL as changeable parent
    AUDIOPLL can be a parrent to many GCLKs. This causes a race on AUDIOPLL,
    since the first audio IP that uses it will set the AUDIOPLL to the lowest
    rate that can be served and would be acceptable by the requesting audio
    IP. This rate might not be good for another audio IP that wants to use the
    AUDIOPLL as its parent, so its clk_set_rate() will return an error.
    Because of this, we remove the ability to change the rate of the AUDIOPLL.
    AUDIOPLL can be set using assigned-clocks to a value that is big enough to
    cover all the audio IPs needs and enable the use of GCLKs divisors.
    
    Signed-off-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- drivers/clk/at91/sama7g5.c
+++ drivers/clk/at91/sama7g5.c
@@ -494,7 +494,7 @@ static const struct {
 	  .pp = { "audiopll_divpmcck", },
 	  .pp_mux_table = { 9, },
 	  .pp_count = 1,
-	  .pp_chg_id = 3, },
+	  .pp_chg_id = INT_MIN, },
 
 	{ .n  = "csi_gclk",
 	  .id = 33,
@@ -638,7 +638,7 @@ static const struct {
 	  .pp = { "syspll_divpmcck", "audiopll_divpmcck", },
 	  .pp_mux_table = { 5, 9, },
 	  .pp_count = 2,
-	  .pp_chg_id = 4, },
+	  .pp_chg_id = INT_MIN, },
 
 	{ .n  = "i2smcc1_gclk",
 	  .id = 58,
@@ -646,7 +646,7 @@ static const struct {
 	  .pp = { "syspll_divpmcck", "audiopll_divpmcck", },
 	  .pp_mux_table = { 5, 9, },
 	  .pp_count = 2,
-	  .pp_chg_id = 4, },
+	  .pp_chg_id = INT_MIN, },
 
 	{ .n  = "mcan0_gclk",
 	  .id = 61,
@@ -812,7 +812,7 @@ static const struct {
 	  .pp = { "syspll_divpmcck", "audiopll_divpmcck", },
 	  .pp_mux_table = { 5, 9, },
 	  .pp_count = 2,
-	  .pp_chg_id = 4, },
+	  .pp_chg_id = INT_MIN, },
 
 	{ .n = "spdiftx_gclk",
 	  .id = 85,
@@ -820,7 +820,7 @@ static const struct {
 	  .pp = { "syspll_divpmcck", "audiopll_divpmcck", },
 	  .pp_mux_table = { 5, 9, },
 	  .pp_count = 2,
-	  .pp_chg_id = 4, },
+	  .pp_chg_id = INT_MIN, },
 
 	{ .n  = "tcb0_ch0_gclk",
 	  .id = 88,
