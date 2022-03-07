383275511c9aa4291bf7f7c103497144ceb60f24
Robert Nelson <robertcnelson@gmail.com>
2022-03-07 16:40:21 -0600

    merge: CONFIG_PREEMPT_RT Patch Set
    patch-4.19.232-rt104.patch.xz
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- include/linux/kthread.h
+++ include/linux/kthread.h
@@ -88,7 +88,7 @@ enum {
 
 struct kthread_worker {
 	unsigned int		flags;
-	spinlock_t		lock;
+	raw_spinlock_t		lock;
 	struct list_head	work_list;
 	struct list_head	delayed_work_list;
 	struct task_struct	*task;
@@ -109,7 +109,7 @@ struct kthread_delayed_work {
 };
 
 #define KTHREAD_WORKER_INIT(worker)	{				\
-	.lock = __SPIN_LOCK_UNLOCKED((worker).lock),			\
+	.lock = __RAW_SPIN_LOCK_UNLOCKED((worker).lock),		\
 	.work_list = LIST_HEAD_INIT((worker).work_list),		\
 	.delayed_work_list = LIST_HEAD_INIT((worker).delayed_work_list),\
 	}
