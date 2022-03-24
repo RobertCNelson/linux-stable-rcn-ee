403cf1ca1b823ffb8f8a764bf057298af0267597
Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
2022-03-24 17:38:16 +0200

    ASoC: dt-bindings: document Microchip's ASRC Sound Card
    Add DT bindings for the new Sound Card that can be used to enable ASRC
    present in sama7g5 SoCs.
    
    Signed-off-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
    (cherry picked from linux-6.1-trunk/at91/sound)
    Signed-off-by: Andrei Simion <andrei.simion@microchip.com>

---
+++ include/dt-bindings/sound/microchip,asrc-card.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_BINDINGS_MICROCHIP_ASRC_CARD_H__
+#define __DT_BINDINGS_MICROCHIP_ASRC_CARD_H__
+
+/* formats to be used for Back-ends of Microhip's ASRC */
+/* must be kept in-sync with include/uapi/sound/asound.h */
+#define MCHP_ASRC_PCM_FORMAT_S8		0
+#define MCHP_ASRC_PCM_FORMAT_S16_LE	2
+#define MCHP_ASRC_PCM_FORMAT_S24_LE	6
+#define MCHP_ASRC_PCM_FORMAT_S32_LE	10
+
+#endif /* __DT_BINDINGS_MICROCHIP_ASRC_CARD_H__ */
