ec9d85bb0692e832cd6adf42686cc6a243420187
Charles Mirabile <cmirabil@redhat.com>
2022-08-23 13:41:54 -0400

    drivers/mfd: sensehat: Add Raspberry Pi Sense HAT to simple_mfd_i2c
    This patch adds the compatible string for the Sense HAT device to
    the list of compatible strings in the simple_mfd_i2c driver so that
    it can match against the device and load its children and their drivers
    
    Co-developed-by: Joel Savitz <jsavitz@redhat.com>
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>
    Signed-off-by: Charles Mirabile <cmirabil@redhat.com>

--- drivers/mfd/simple-mfd-i2c.c
+++ drivers/mfd/simple-mfd-i2c.c
@@ -75,6 +75,7 @@ static const struct simple_mfd_data silergy_sy7636a = {
 static const struct of_device_id simple_mfd_i2c_of_match[] = {
 	{ .compatible = "kontron,sl28cpld" },
 	{ .compatible = "silergy,sy7636a", .data = &silergy_sy7636a},
+	{ .compatible = "raspberrypi,sensehat" },
 	{}
 };
 MODULE_DEVICE_TABLE(of, simple_mfd_i2c_of_match);
