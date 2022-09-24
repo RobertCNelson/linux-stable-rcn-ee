2dcc2dc93dfbb8f46d9107bbab77962045d4220f
Robert Nelson <robertcnelson@gmail.com>
2022-09-23 19:12:40 -0500

    merge: aufs-standalone
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -1423,6 +1423,7 @@ rename_retry:
 	seq = 1;
 	goto again;
 }
+EXPORT_SYMBOL_GPL(d_walk);
 
 struct check_mount {
 	struct vfsmount *mnt;
@@ -2968,6 +2969,7 @@ void d_exchange(struct dentry *dentry1, struct dentry *dentry2)
 
 	write_sequnlock(&rename_lock);
 }
+EXPORT_SYMBOL_GPL(d_exchange);
 
 /**
  * d_ancestor - search for an ancestor
