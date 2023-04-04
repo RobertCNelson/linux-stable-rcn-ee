e1403cb3652b75de96fa02dc1acc1c6330537258
Matthew Brost <matthew.brost@intel.com>
2023-04-03 17:22:03 -0700

    drm/sched: Move schedule policy to scheduler / entity
    Rather than a global modparam for scheduling policy, move the scheduling
    policy to scheduler / entity so user can control each scheduler / entity
    policy.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/etnaviv/etnaviv_sched.c
+++ drivers/gpu/drm/etnaviv/etnaviv_sched.c
@@ -137,7 +137,8 @@ int etnaviv_sched_init(struct etnaviv_gpu *gpu)
 	ret = drm_sched_init(&gpu->sched, &etnaviv_sched_ops, NULL,
 			     etnaviv_hw_jobs_limit, etnaviv_job_hang_limit,
 			     msecs_to_jiffies(500), NULL, NULL,
-			     dev_name(gpu->dev), gpu->dev);
+			     dev_name(gpu->dev), DRM_SCHED_POLICY_DEFAULT,
+			     gpu->dev);
 	if (ret)
 		return ret;
 
