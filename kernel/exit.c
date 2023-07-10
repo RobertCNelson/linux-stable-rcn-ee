c2e92f82a426fc1d78ce7d17d1a140fe8a9e4819
Robert Nelson <robertcnelson@gmail.com>
2023-07-10 17:59:41 -0500

    merge: CONFIG_PREEMPT_RT Patch Set
    patch-5.15.119-rt65.patch.xz
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -64,6 +64,7 @@
 #include <linux/rcuwait.h>
 #include <linux/compat.h>
 #include <linux/io_uring.h>
+#include <linux/kprobes.h>
 #include <linux/sysfs.h>
 
 #include <linux/uaccess.h>
@@ -215,8 +216,14 @@ static void delayed_put_task_struct(struct rcu_head *rhp)
 {
 	struct task_struct *tsk = container_of(rhp, struct task_struct, rcu);
 
+	kprobe_flush_task(tsk);
 	perf_event_delayed_put(tsk);
 	trace_sched_process_free(tsk);
+
+	/* RT enabled kernels delay freeing the VMAP'ed task stack */
+	if (IS_ENABLED(CONFIG_PREEMPT_RT))
+		put_task_stack(tsk);
+
 	put_task_struct(tsk);
 }
 
