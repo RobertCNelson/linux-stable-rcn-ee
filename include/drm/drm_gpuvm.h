a325a2c6119ff919612a2a5fa73f94ec63c3c991
Donald Robson <donald.robson@imgtec.com>
2023-10-10 14:37:20 +0100

    drm/gpuvm: Helper to get range of unmap from a remap op.
    Determining the start and range of the unmap stage of a remap op is a
    common piece of code currently implemented by multiple drivers. Add a
    helper for this.
    
    Changes since v6:
    - Remove use of __always_inline
    
    Signed-off-by: Donald Robson <donald.robson@imgtec.com>
    Signed-off-by: Sarah Walker <sarah.walker@imgtec.com>

--- include/drm/drm_gpuvm.h
+++ include/drm/drm_gpuvm.h
@@ -702,4 +702,31 @@ void drm_gpuva_remap(struct drm_gpuva *prev,
 
 void drm_gpuva_unmap(struct drm_gpuva_op_unmap *op);
 
+/**
+ * drm_gpuva_op_remap_get_unmap_range() - Helper to get the start and range of
+ * the unmap stage of a remap op.
+ * @op: Remap op.
+ * @start_addr: Output pointer for the start of the required unmap.
+ * @range: Output pointer for the length of the required unmap.
+ *
+ * These parameters can then be used by the caller to unmap memory pages that
+ * are no longer required.
+ */
+static inline void
+drm_gpuva_op_remap_get_unmap_range(const struct drm_gpuva_op_remap *op,
+				   u64 *start_addr, u64 *range)
+{
+	const u64 va_start = op->prev ?
+			     op->prev->va.addr + op->prev->va.range :
+			     op->unmap->va->va.addr;
+	const u64 va_end = op->next ?
+			   op->next->va.addr :
+			   op->unmap->va->va.addr + op->unmap->va->va.range;
+
+	if (start_addr)
+		*start_addr = va_start;
+	if (range)
+		*range = va_end - va_start;
+}
+
 #endif /* __DRM_GPUVM_H__ */
