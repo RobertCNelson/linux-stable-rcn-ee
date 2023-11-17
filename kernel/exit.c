c1b97002da623c1386fee3f408a8cc77065143ce
Robert Nelson <robertcnelson@gmail.com>
2023-11-17 16:07:41 -0600

    merge: CONFIG_PREEMPT_RT Patch Set
    patch-5.4.257-rt87.patch.xz
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -208,7 +208,7 @@ static void __exit_signal(struct task_struct *tsk)
 	 * Do this under ->siglock, we can race with another thread
 	 * doing sigqueue_free() if we have SIGQUEUE_PREALLOC signals.
 	 */
-	flush_sigqueue(&tsk->pending);
+	flush_task_sigqueue(tsk);
 	tsk->sighand = NULL;
 	spin_unlock(&sighand->siglock);
 
@@ -305,6 +305,7 @@ void rcuwait_wake_up(struct rcuwait *w)
 		wake_up_process(task);
 	rcu_read_unlock();
 }
+EXPORT_SYMBOL_GPL(rcuwait_wake_up);
 
 /*
  * Determine if a process group is "orphaned", according to the POSIX
