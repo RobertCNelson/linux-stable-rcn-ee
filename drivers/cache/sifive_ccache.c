57bd3ba573c569185e197c32af4a532482e14dc2
Conor Dooley <conor.dooley@microchip.com>
2024-03-04 11:42:48 +0000

    cache: ccache: add mpfs to nonstandard cache ops list
    On PolarFire SoC, for performance reasons, we want to use non-coherent
    DMA. Add it to the match table with the non-standard non-coherent
    cache ops requirement.
    
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- drivers/cache/sifive_ccache.c
+++ drivers/cache/sifive_ccache.c
@@ -120,6 +120,8 @@ static const struct of_device_id sifive_ccache_ids[] = {
 	{ .compatible = "sifive,fu740-c000-ccache" },
 	{ .compatible = "starfive,jh7100-ccache",
 	  .data = (void *)(QUIRK_NONSTANDARD_CACHE_OPS | QUIRK_BROKEN_DATA_UNCORR) },
+	{ .compatible = "microchip,mpfs-ccache",
+	  .data = (void *)(QUIRK_NONSTANDARD_CACHE_OPS) },
 	{ .compatible = "sifive,ccache0" },
 	{ /* end of table */ }
 };
