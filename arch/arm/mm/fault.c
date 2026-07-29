06b72c9e060b77ef2fc4f19803823724eba6f6d2
Robert Nelson <robertcnelson@gmail.com>
2026-07-29 12:23:03 -0500

    merge: CONFIG_PREEMPT_RT Patch Set
    patch-6.6.144-rt76.patch.xz
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -451,6 +451,9 @@ do_translation_fault(unsigned long addr, unsigned int fsr,
 	if (addr < TASK_SIZE)
 		return do_page_fault(addr, fsr, regs);
 
+	if (interrupts_enabled(regs))
+		local_irq_enable();
+
 	if (user_mode(regs))
 		goto bad_area;
 
@@ -522,6 +525,8 @@ do_translation_fault(unsigned long addr, unsigned int fsr,
 static int
 do_sect_fault(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 {
+	if (interrupts_enabled(regs))
+		local_irq_enable();
 	/*
 	 * If this is a kernel address, but from user mode, then userspace
 	 * is trying bad stuff. Invoke the branch predictor handling.
